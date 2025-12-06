31f4e4aacc977011bee39bf86b5506fdf9652b37
Dennis1725 <dennismika67@gmail.com>
2025-12-06 17:01:44 +0100

    ops: add code coverage workflow

--- src/test/java/com/hse/leihsy/LeihsyApplicationTests.java
+++ src/test/java/com/hse/leihsy/LeihsyApplicationTests.java
@@ -2,15 +2,17 @@ package com.hse.leihsy;
 
 import com.hse.leihsy.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@EnableAutoConfiguration(exclude = {OAuth2ResourceServerAutoConfiguration.class})
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		properties = {
+				"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration"
+		}
+)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Import(TestSecurityConfig.class)
